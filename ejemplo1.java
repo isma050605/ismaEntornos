6e7ea24590a94d0f0d6d0401436d19b18ca75e5b
isma050605 <ismaelbuaisa@gmail.com>
2024-02-20 10:00:56 +0100

    Los primeros cambios de los ejemplos 1 y 2 desde rama jcisma

--- ejemplo1.java
+++ ejemplo1.java
@@ -1,16 +1,24 @@
 package entornosTareaGit;
-
+import java.util.Scanner;
 public class ejemplo1{
     private String mensaje;
     private String ip;
+    private String nombre;
+    private String apellido;
 
-    public ejemplo1(String mensaje) {
-        this.mensaje = mensaje;
-    }
 
-    public void imprimirMensaje() {
+    public ejemplo1(String mensaje, String ip, String nombre, String apellido) {
+		super();
+		this.mensaje = mensaje;
+		this.ip = ip;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	public void imprimirMensaje() {
         System.out.println("Mensaje desde ClaseUno: " + mensaje);
         System.out.println("Otro cambio mas");
+        System.out.println("Este es el primer cambio del ejemplo jcisma desde la rama jcisma");
     }
 }
 
--- ejemplo2.java
+++ ejemplo2.java
@@ -1,16 +1,35 @@
 package entornosTareaGit;
-
+import java.util.ArrayList;
 public class ejemplo2 {
 	    private int numero;
 	    private int numero2;
-private int id;
-	    public ejemplo2(int numero) {
-	        this.numero = numero;
-	    }
+	    private int numero3;
+        private int id;
+        private String direccion;
+	   
+public ejemplo2(int numero, int numero2, int numero3, int id, String direccion) {
+			super();
+			this.numero = numero;
+			this.numero2 = numero2;
+			this.numero3 = numero3;
+			this.id = id;
+			this.direccion = direccion;
+		}
 
+public int getNumero() {
+	return numero;
+}
+public String getDireccion() {
+	return direccion;
+}
+
+private void suma() {
+	System.out.println("2+2=4");
+}
 	    public void imprimirNumero() {
 	        System.out.println("Número desde ClaseDos: " + numero);
 	        System.out.println("Otro cambio");
+	        
 	    }
 	}
 
