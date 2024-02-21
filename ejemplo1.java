fe4b339ff5e9f30679b90bab57f92093c435fdab
isma050605 <ismaelbuaisa@gmail.com>
2024-02-21 10:28:31 +0100

    Este es el segundo cambio en los ejemplos 1 y 2 desde la rama jcisma

--- ejemplo1.java
+++ ejemplo1.java
@@ -1,24 +1,37 @@
 package entornosTareaGit;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class ejemplo1{
     private String mensaje;
     private String ip;
     private String nombre;
     private String apellido;
+    private String entornos;
 
 
-    public ejemplo1(String mensaje, String ip, String nombre, String apellido) {
+    public ejemplo1(String mensaje, String ip, String nombre, String apellido,String entornos) {
 		super();
 		this.mensaje = mensaje;
 		this.ip = ip;
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.entornos = entornos;
 	}
+    
+
+	@Override
+	public String toString() {
+		return "ejemplo1 [mensaje=" + mensaje + ", ip=" + ip + ", nombre=" + nombre + ", apellido=" + apellido
+				+ ", entornos=" + entornos + "]";
+	}
+
 
 	public void imprimirMensaje() {
         System.out.println("Mensaje desde ClaseUno: " + mensaje);
         System.out.println("Otro cambio mas");
         System.out.println("Este es el primer cambio del ejemplo jcisma desde la rama jcisma");
+        System.out.println("Este es el segundo cambio del ejemplo 1 desde la rama jcisma");
     }
 }
 
--- ejemplo2.java
+++ ejemplo2.java
@@ -1,11 +1,14 @@
 package entornosTareaGit;
 import java.util.ArrayList;
+import java.util.Objects;
 public class ejemplo2 {
 	    private int numero;
 	    private int numero2;
 	    private int numero3;
         private int id;
         private String direccion;
+        private int idOficina;
+        private String ddd;
 	   
 public ejemplo2(int numero, int numero2, int numero3, int id, String direccion) {
 			super();
@@ -15,6 +18,26 @@ public ejemplo2(int numero, int numero2, int numero3, int id, String direccion) 
 			this.id = id;
 			this.direccion = direccion;
 		}
+
+
+@Override
+public int hashCode() {
+	return Objects.hash(direccion);
+}
+
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	ejemplo2 other = (ejemplo2) obj;
+	return Objects.equals(direccion, other.direccion);
+}
+
 
 public int getNumero() {
 	return numero;
@@ -29,6 +52,7 @@ private void suma() {
 	    public void imprimirNumero() {
 	        System.out.println("Número desde ClaseDos: " + numero);
 	        System.out.println("Otro cambio");
+	        System.out.println("Este es el segundo cambio en el ejemplo 2 desde la rama jcisma");
 	        
 	    }
 	}
