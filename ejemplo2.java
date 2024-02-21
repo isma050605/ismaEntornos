8d8f681a16f51a3cbec78a960f76d28ad310a9e7
isma050605 <ismaelbuaisa@gmail.com>
2024-02-21 11:01:45 +0100

    Este es el primer cambio de los ejemplos 2 y 3 desde la rama master

--- ejemplo2.java
+++ ejemplo2.java
@@ -1,17 +1,35 @@
 package entornosTareaGit;
 
 public class ejemplo2 {
-	    private int numero;
-	    private int numero2;
-private int id;
-	    public ejemplo2(int numero) {
-	        this.numero = numero;
-	    }
+    private int numero;
+    private int numero2;
+    private int id;
 
-	    public void imprimirNumero() {
-	        System.out.println("Número desde ClaseDos: " + numero);
-	        System.out.println("Otro cambio");
-	    }
-	}
+    public ejemplo2(int numero) {
+        this.numero = numero;
+    }
 
+    public void imprimirNumero() {
+        System.out.println("Número desde ClaseDos: " + numero);
+        System.out.println("Otro cambio");
+        System.out.println("Este es el primer cambio del ejemplo2 desde la rama master");
+    }
 
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+public void Isma() {
+	System.out.println("Isma");
+}
+    public int getNumero2() {
+        return numero2;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
--- ejemplo3.java
+++ ejemplo3.java
@@ -1,17 +1,40 @@
 package entornosTareaGit;
 
 public class ejemplo3 {
-	    private boolean estado;
-	    private boolean valido;
-private boolean acertado;
-	    public ejemplo3(boolean estado) {
-	        this.estado = estado;
-	    }
+    private int numero;
+    private int numero2;
+    private int id;
+    public ejemplo3(int numero) {
+        this.numero = numero;
+    }
+    public void imprimirNumero() {
+        System.out.println("Número desde ClaseDos: " + numero);
+        System.out.println("Otro cambio");
+        System.out.println("Este el primer cambio en el ejemplo 3 desde la rama master");
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public int getNumero2() {
+        return numero2;
+    }
 
-	    public void imprimirEstado() {
-	        System.out.println("Estado desde ClaseTres: " + estado);
-	        System.out.println("Nuevo cambio");
-	    }
-	}
+    public void setNumero2(int numero2) {
+        this.numero2 = numero2;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void JuanCarlos() {
+    	System.out.println("JuanCarlos");
+    }
+    
+    public int calcularSuma() {
+        return numero + numero2;
+    }
+}
+
 
 
