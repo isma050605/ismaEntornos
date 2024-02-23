f30a6e030f8f85ab925bd1a9d3e92721de8b206b
isma050605 <ismaelbuaisa@gmail.com>
2024-02-23 19:09:41 +0100

    Este es el segundo commit en los ejemplos 2 y 3 desde la rama master

--- ejemplo2.java
+++ ejemplo2.java
@@ -1,5 +1,8 @@
 package entornosTareaGit;
-
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Dictionary;
+import java.util.function.*;
 public class ejemplo2 {
     private int numero;
     private int numero2;
@@ -13,6 +16,7 @@ public class ejemplo2 {
         System.out.println("Número desde ClaseDos: " + numero);
         System.out.println("Otro cambio");
         System.out.println("Este es el primer cambio del ejemplo2 desde la rama master");
+        System.out.println("Este es el segundo cambio del ejemplo 2 desde la rama master");
     }
 
     public int getNumero() {
@@ -22,11 +26,18 @@ public class ejemplo2 {
     public void setNumero(int numero) {
         this.numero = numero;
     }
+    
+    public boolean esPar(int num) {
+        return num % 2 == 0;
+    }
 public void Isma() {
 	System.out.println("Isma");
 }
     public int getNumero2() {
         return numero2;
+    }
+    public int calcularCuadrado(int num) {
+        return num * num;
     }
 
     public void setId(int id) {
--- ejemplo3.java
+++ ejemplo3.java
@@ -1,20 +1,28 @@
 package entornosTareaGit;
+import java.util.Random;
 
 public class ejemplo3 {
     private int numero;
     private int numero2;
     private int id;
+
     public ejemplo3(int numero) {
         this.numero = numero;
+        Random rand = new Random();
+        this.id = rand.nextInt(1000); // Genera un número aleatorio entre 0 y 999 y lo asigna a id
     }
+
     public void imprimirNumero() {
         System.out.println("Número desde ClaseDos: " + numero);
         System.out.println("Otro cambio");
         System.out.println("Este el primer cambio en el ejemplo 3 desde la rama master");
+        System.out.println("Este es el segundo cambio en el ejemplo 3 desde la rama master");
     }
+
     public int getNumero() {
         return numero;
     }
+
     public int getNumero2() {
         return numero2;
     }
@@ -23,18 +31,16 @@ public class ejemplo3 {
         this.numero2 = numero2;
     }
 
-    public int getId() {
-        return id;
+    public void JuanCarlos() {
+        System.out.println("JuanCarlos");
     }
 
-    public void JuanCarlos() {
-    	System.out.println("JuanCarlos");
-    }
-    
     public int calcularSuma() {
         return numero + numero2;
     }
+
+    // Nuevo método para calcular el producto de los dos números
+    public int calcularProducto() {
+        return numero * numero2;
+    }
 }
-
-
-
