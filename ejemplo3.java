396cc9d6d5d1134200fe21ae0c1f8884134ebfb3
JCREYES18 <159776432+JCREYES18@users.noreply.github.com>
2024-03-25 17:17:04 +0100

    primerCommitGitHub ejemplo3.java

--- ejemplo3.java
+++ ejemplo3.java
@@ -1,15 +1,12 @@
-package entornosTareaGit;
 import java.util.Random;
 
 public class ejemplo3 {
     private int numero;
-    private int numero2;
     private int id;
 
     public ejemplo3(int numero) {
         this.numero = numero;
-        Random rand = new Random();
-        this.id = rand.nextInt(1000); // Genera un número aleatorio entre 0 y 999 y lo asigna a id
+        generarIdAleatorio();
     }
 
     public void imprimirNumero() {
@@ -23,24 +20,17 @@ public class ejemplo3 {
         return numero;
     }
 
-    public int getNumero2() {
-        return numero2;
-    }
-
-    public void setNumero2(int numero2) {
-        this.numero2 = numero2;
-    }
-
     public void JuanCarlos() {
         System.out.println("JuanCarlos");
     }
 
-    public int calcularSuma() {
+    public int calcularSuma(int numero2) {
         return numero + numero2;
     }
 
-    // Nuevo método para calcular el producto de los dos números
-    public int calcularProducto() {
-        return numero * numero2;
+    // Nuevo método para generar un ID aleatorio
+    private void generarIdAleatorio() {
+        Random rand = new Random();
+        id = rand.nextInt(1000); // Genera un número aleatorio entre 0 y 999 y lo asigna a id
     }
 }
