ca1e7f43791a169a9eb1ce86190a6c889daaffc1
JCREYES18 <159776432+JCREYES18@users.noreply.github.com>
2024-03-25 17:10:25 +0100

    cuartoCommitGitHub ejemplo2.java

--- ejemplo2.java
+++ ejemplo2.java
@@ -6,50 +6,31 @@ public class ejemplo2 {
     private int numero;
     private String direccion;
 
-    public ejemplo2(int numero) {
+    public ejemplo2(int numero, String direccion) {
         this.numero = numero;
+        this.direccion = direccion;
     }
 
     public void imprimirNumero() {
         System.out.println("Número: " + numero);
-        System.out.println("Se ha impreso el número correctamente.");
-        System.out.println("Este es el segundo cambio en el ejemplo 2.");
-        System.out.println("Se ha finalizado la impresión del número.");
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Se ha impreso el número y la dirección correctamente.");
     }
 
-    public boolean esPar() {
-        return numero % 2 == 0;
-    }
-
-    public int calcularCuadrado() {
-        return numero * numero;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
+    public boolean esPositivo() {
+        return numero > 0;
     }
 
     public String getDireccion() {
         return direccion;
     }
 
-    public void modificarDireccion(String nuevaDireccion) {
+    public void cambiarDireccion(String nuevaDireccion) {
         direccion = nuevaDireccion;
         System.out.println("La dirección ha sido modificada correctamente.");
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(direccion);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null || getClass() != obj.getClass())
-            return false;
-        ejemplo2 other = (ejemplo2) obj;
-        return Objects.equals(direccion, other.direccion);
+    public boolean mismaDireccion(ejemplo2 otroEjemplo) {
+        return Objects.equals(direccion, otroEjemplo.direccion);
     }
 }
