50b2a70246730e6de9f96e65941c978c60796171
JCREYES18 <159776432+JCREYES18@users.noreply.github.com>
2024-03-25 16:39:11 +0100

    tercerCommitGitHub ejemplo1.java

--- ejemplo1.java
+++ ejemplo1.java
@@ -1,8 +1,6 @@
 package entornosTareaGit;
 import java.util.Scanner;
 import java.util.Objects;
-//hola
-
 
 public class ejemplo1{
     private String mensaje;
@@ -11,6 +9,7 @@ public class ejemplo1{
     private String apellido;
     private String entornos;
     private int euro;
+    
     public ejemplo1(String mensaje, String ip, String nombre, String apellido,String entornos) {
 		super();
 		this.mensaje = mensaje;
@@ -19,22 +18,26 @@ public class ejemplo1{
 		this.apellido = apellido;
 		this.entornos = entornos;
 	}
+	
 	@Override
 	public String toString() {
 		return "ejemplo1 [mensaje=" + mensaje + ", ip=" + ip + ", nombre=" + nombre + ", apellido=" + apellido
 				+ ", entornos=" + entornos + "]";
 	}
-	 public static void solicitarYMostrarNombre() {
+	
+	public static void solicitarYMostrarNombre() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Por favor, ingresa tu nombre: ");
         String nombre = scanner.nextLine();
         System.out.println("Hola, " + nombre + "! Bienvenido.");
         scanner.close();
     }
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(apellido, entornos, nombre);
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -47,25 +50,29 @@ public class ejemplo1{
 		return Objects.equals(apellido, other.apellido) && Objects.equals(entornos, other.entornos)
 				&& Objects.equals(nombre, other.nombre);
 	}
-	public class Suma {
+	
+	public static int calcularFactorial(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("El número no puede ser negativo.");
+        }
+        int factorial = 1;
+        for (int i = 2; i <= num; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+    
     public static void main(String[] args) {
-        int numero1 = 30;
-        int numero2 = 10000000;
-        int resultado = sumarNumeros(numero1, numero2);
-        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + resultado);
+        int numero = 5;
+        int factorial = calcularFactorial(numero);
+        System.out.println("El factorial de " + numero + " es: " + factorial);
     }
-
-    public static int sumarNumeros(int num1, int num2) {
-        return num1 + num2;
-    }
-}
-//hola
     
     public void imprimirMensaje2() {
         System.out.println("Mensaje desde ClaseUno: " + mensaje);
         System.out.println("Otro cambio mas");
         System.out.println("Otro cambio mas");
     }
-    
 }
+
 
